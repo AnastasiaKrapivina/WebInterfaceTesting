0c6dcbdedbbcc700b7c3d13478b423006c17fcb6
Anastasia Krapivina <krapivina934@gmail.com>
2023-06-23 18:21:32 +0700

    all tests

--- src/test/java/ru/netology/web/CardOrderFormsTest.java
+++ src/test/java/ru/netology/web/CardOrderFormsTest.java
@@ -27,7 +27,7 @@ public class CardOrderFormsTest {
         form.$("[data-test-id=phone] input").setValue("+79999990000");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
     @Test
     void shouldTestNegativePhone() {
@@ -37,6 +37,6 @@ public class CardOrderFormsTest {
         form.$("[data-test-id=phone] input").setValue("+7999999000");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        $(".input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 }
