6257907ff4e40199cbbf544fe6d66945f821b46c
Anastasia Krapivina <krapivina934@gmail.com>
2023-06-23 18:00:29 +0700

    add tests

--- src/test/java/ru/netology/web/CardOrderFormsTest.java
+++ src/test/java/ru/netology/web/CardOrderFormsTest.java
@@ -1,15 +1,42 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.*;
 
 
 public class CardOrderFormsTest {
     @Test
     void shouldTest() {
-        open("http://0.0.0.0:9999");
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Анастасия");
+        form.$("[data-test-id=phone] input").setValue("+79999990000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
 
     }
-
+    @Test
+    void shouldTestNegativeName() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Anastasia");
+        form.$("[data-test-id=phone] input").setValue("+79999990000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+    @Test
+    void shouldTestNegativePhone() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Анастасия");
+        form.$("[data-test-id=phone] input").setValue("+7999999000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $(".input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
 }
